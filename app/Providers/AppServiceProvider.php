d13e4049e067e5a37d90a9b1ac3ff0ad8f6ed728
tsabitah <tsabitahhilyatul@gmail.com>
2026-08-05 21:00:20 +0700

    supaya bisa deploy :)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,5 +18,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
