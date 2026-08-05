61a53f30d4dfe888c3c516c8f2031a172032c82f
tsabitah <tsabitahhilyatul@gmail.com>
2026-08-06 04:24:03 +0700

    proses deploy API

--- atmobrass-api/app.py
+++ atmobrass-api/app.py
@@ -774,5 +774,8 @@ def forecast_model():
         }), 500
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
